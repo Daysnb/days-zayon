9c84a15bbef7645b9c1029de946d2b3d783809ac
Days <aleatoriamente22ll@gmail.com>
2026-05-20 16:48:12 -0300

    Enhance comments and fix typo in rendimento print
    Added a comment to clarify the purpose of the print statement and corrected a typo in the print statement for the rendimento.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from conta import Conta
 conta = Conta()
 
 while true:
-  print() #so pra pular uma linhazinha :)#
+  print() #so pra pular uma linhazinha :) #genial
   print("1. Depositar")
   print("2. Sacar")
   print("3. Ver saldo")
@@ -22,7 +22,10 @@ elif opcao == "3":
   print("Saldo:", conta.saldo)
 
 elif opcao == "4":
-  print("Rendimento:", conta.calcula_rendimento()) #É calcula_rendimento msm :)#
+  print("Rendimento:", conta.calcula_rendimento()) #É calcula_rendimento msm :) #W
 
 else:
   ("Opção invalida, por favor tente novamente.")
+
+
+#belo codigo zigoto
