8240c854933806a316a8183cbe725a5a1f5bb549
Days <aleatoriamente22ll@gmail.com>
2026-05-20 16:18:14 -0300

    codigo POO
    classe conta, saque, deposito e calcular rendimento.

--- conta.py
+++ conta.py
@@ -1,1 +1,20 @@
+class Conta:
+  def __init(self):
+    self.saldo = 0
 
+def saca(self, valor):
+  self.saldo -= valor 
+
+def deposite(self, valor):
+  self.saldo += valor 
+
+def calcula_rendimento(self):
+  return self.saldo * 0.1
+
+conta = conta()
+
+conta.deposite(100)
+
+print(conta.saldo)
+
+print(conta.calcula_rendimento())
