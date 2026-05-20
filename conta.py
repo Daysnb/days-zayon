17d46f11d5de4db300b43469e9a11128229a1a46
Zaio <assuncaozayon@gmail.com>
2026-05-20 16:20:20 -0300

    elogiei o parceiro
    codigo mt bom man slk

--- conta.py
+++ conta.py
@@ -2,14 +2,14 @@ class Conta:
   def __init(self):
     self.saldo = 0
 
-def saca(self, valor):
-  self.saldo -= valor 
+  def saca(self, valor):
+    self.saldo -= valor 
 
-def deposite(self, valor):
-  self.saldo += valor 
+  def deposite(self, valor):
+    self.saldo += valor 
 
-def calcula_rendimento(self):
-  return self.saldo * 0.1
+  def calcula_rendimento(self):
+    return self.saldo * 0.1
 
 conta = conta()
 
@@ -18,3 +18,7 @@ conta.deposite(100)
 print(conta.saldo)
 
 print(conta.calcula_rendimento())
+
+
+
+codigo mt bom man parabens :)
